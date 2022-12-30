210f78f12ba2766943d66d36599433ec0c803479
JeromeCervelle <cervelle.jerome@educ-valadon-limoges.fr>
2022-12-30 17:15:57 +0100

    Initial commit

---
+++ src/main/java/com/hrapplicationweb2/HrApplicationWeb2Application.java
@@ -0,0 +1,13 @@
+package com.hrapplicationweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApplicationWeb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrApplicationWeb2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrapplicationweb2/HrApplicationWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrapplicationweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApplicationWeb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
